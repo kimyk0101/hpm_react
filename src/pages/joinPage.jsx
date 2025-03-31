bf9971d3e665525c1018c493cffef22704cefd30
서민정 <ybsm0629@gmail.com>
2025-03-31 15:32:45 +0900

    날씨/회원가입 주소
    1. 산 위치별 날씨
    2. 회원가입시 : 주소목록

--- src/pages/joinPage.jsx
+++ src/pages/joinPage.jsx
@@ -24,7 +24,7 @@ const JoinPage = () => {
     birthYear: "",
     birthMonth: "",
     birthDay: "",
-    phoneNumber: "",  
+    phoneNumber: "",
     email: "",
     address: "",
     updateDate: new Date().toISOString(), // 현재 날짜로 초기화
@@ -51,20 +51,22 @@ const JoinPage = () => {
       alert("아이디는 영문, 숫자 조합, 6자 이상이어야 합니다.");
       return;
     }
-  
+
     const requestUrl = `http://localhost:8088/api/users/check-user-id?userId=${formData.userId}`;
     console.log("아이디 중복 확인 요청 URL:", requestUrl);
-  
+
     try {
       const response = await fetch(requestUrl);
-  
+
       if (!response.ok) {
-        throw new Error(`서버 응답 오류: ${response.statusText} (상태 코드: ${response.status})`);
+        throw new Error(
+          `서버 응답 오류: ${response.statusText} (상태 코드: ${response.status})`
+        );
       }
-  
+
       const isTaken = await response.json();
       setIsUsernameAvailable(isTaken);
-  
+
       if (isTaken) {
         alert("사용 가능한 아이디입니다.");
       } else {
@@ -78,16 +80,21 @@ const JoinPage = () => {
 
   const handlePasswordChange = (e) => {
     const password = e.target.value;
-    
+
     setFormData((prevState) => ({
       ...prevState,
       password,
     }));
-  
+
     // 비밀번호 강도 체크
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    setPasswordStrength(passwordRegex.test(password) ? "보안성 : 강함" : "비밀번호는 영문, 숫자, 특수문자를 포함해 8자 이상이어야 합니다.");
-    
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    setPasswordStrength(
+      passwordRegex.test(password)
+        ? "보안성 : 강함"
+        : "비밀번호는 영문, 숫자, 특수문자를 포함해 8자 이상이어야 합니다."
+    );
+
     // 비밀번호 확인과 즉시 비교
     setPasswordMatch(password === formData.confirmPassword);
   };
@@ -99,20 +106,20 @@ const JoinPage = () => {
       setPasswordMatch(formData.password === formData.confirmPassword);
     }
   }, [formData.password, formData.confirmPassword]);
-  
+
   const handleConfirmPasswordChange = (e) => {
     const confirmPassword = e.target.value;
-  
+
     // 기존 formData 업데이트
     setFormData((prevState) => ({
       ...prevState,
       confirmPassword,
     }));
-  
+
     // 입력된 값과 즉시 비교 (formData.password 대신 직접 참조)
     setPasswordMatch(confirmPassword === formData.password);
   };
-  
+
   const handleDateChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -120,13 +127,28 @@ const JoinPage = () => {
 
   const handleSignup = async (e) => {
     e.preventDefault();
-  
+
     // 생년월일을 'yyyy-MM-dd' 형식으로 포맷
-    const birth = `${formData.birthYear}-${String(formData.birthMonth).padStart(2, '0')}-${String(formData.birthDay).padStart(2, '0')}`;
+    const birth = `${formData.birthYear}-${String(formData.birthMonth).padStart(
+      2,
+      "0"
+    )}-${String(formData.birthDay).padStart(2, "0")}`;
 
     // formDataToSendWithBirth를 정의
-    const { confirmPassword, userId, phoneNumber, updateDate, ...formDataToSend } = formData;
-    const formDataToSendWithBirth = { ...formDataToSend, birth, user_id: userId, phone_number:phoneNumber, update_date: updateDate }; // 오늘 날짜 추가
+    const {
+      confirmPassword,
+      userId,
+      phoneNumber,
+      updateDate,
+      ...formDataToSend
+    } = formData;
+    const formDataToSendWithBirth = {
+      ...formDataToSend,
+      birth,
+      user_id: userId,
+      phone_number: phoneNumber,
+      update_date: updateDate,
+    }; // 오늘 날짜 추가
 
     // 회원가입 유효성 검사
     if (!formData.name) {
@@ -145,7 +167,10 @@ const JoinPage = () => {
       alert("비밀번호가 일치하지 않습니다.");
       return;
     }
-    if (!formData.email || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email)) {
+    if (
+      !formData.email ||
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email)
+    ) {
       alert("올바른 이메일 형식을 입력하세요.");
       return;
     }
@@ -162,7 +187,7 @@ const JoinPage = () => {
         },
         body: JSON.stringify(formDataToSendWithBirth),
       });
-  
+
       if (response.ok) {
         alert("회원가입 성공! 로그인 페이지로 이동합니다.");
         navigate("/login");
@@ -174,6 +199,23 @@ const JoinPage = () => {
       console.error("회원가입 오류:", error);
       alert(`회원가입 중 오류가 발생했습니다: ${error.message}`);
     }
+  };
+
+  // 회원가입시 : 주소 입력칸 누르면 -> 주소api 불러오기기
+  const handleAddressClick = () => {
+    new window.daum.Postcode({
+      oncomplete: (data) => {
+        let fullAddress = data.roadAddress; // 도로명 주소
+        if (data.jibunAddress) {
+          fullAddress += ` (${data.jibunAddress})`; // 지번 주소 추가
+        }
+
+        setFormData((prevState) => ({
+          ...prevState,
+          address: fullAddress,
+        }));
+      },
+    }).open();
   };
 
   return (
@@ -207,9 +249,7 @@ const JoinPage = () => {
           name="userId"
           placeholder="2~10자 특수문자 제외"
           value={formData.userId}
-          onChange={(e) =>
-            setFormData({ ...formData, userId: e.target.value })
-          }
+          onChange={(e) => setFormData({ ...formData, userId: e.target.value })}
         />
         <button type="button" onClick={checkUserIdAvailability}>
           중복 확인
@@ -241,7 +281,11 @@ const JoinPage = () => {
         )}
         <label>생년월일:</label>
         <div className="birth-select">
-          <select name="birthYear" value={formData.birthYear} onChange={handleDateChange}>
+          <select
+            name="birthYear"
+            value={formData.birthYear}
+            onChange={handleDateChange}
+          >
             <option value="">년</option>
             {years.map((year) => (
               <option key={year} value={year}>
@@ -249,7 +293,11 @@ const JoinPage = () => {
               </option>
             ))}
           </select>
-          <select name="birthMonth" value={formData.birthMonth} onChange={handleDateChange}>
+          <select
+            name="birthMonth"
+            value={formData.birthMonth}
+            onChange={handleDateChange}
+          >
             <option value="">월</option>
             {months.map((month) => (
               <option key={month} value={month}>
@@ -257,7 +305,11 @@ const JoinPage = () => {
               </option>
             ))}
           </select>
-          <select name="birthDay" value={formData.birthDay} onChange={handleDateChange}>
+          <select
+            name="birthDay"
+            value={formData.birthDay}
+            onChange={handleDateChange}
+          >
             <option value="">일</option>
             {days.map((day) => (
               <option key={day} value={day}>
@@ -271,7 +323,9 @@ const JoinPage = () => {
           type="text"
           placeholder="전화번호"
           value={formData.phoneNumber}
-          onChange={(e) => setFormData({ ...formData, phoneNumber: e.target.value })}
+          onChange={(e) =>
+            setFormData({ ...formData, phoneNumber: e.target.value })
+          }
         />
         <label>이메일:</label>
         <input
@@ -283,9 +337,10 @@ const JoinPage = () => {
         <label>주소:</label>
         <input
           type="text"
-          placeholder="주소"
+          placeholder="주소 검색"
           value={formData.address}
-          onChange={(e) => setFormData({ ...formData, address: e.target.value })}
+          onClick={handleAddressClick} // 클릭 시 주소 검색 실행
+          readOnly // 직접 입력 방지
         />
         <button type="submit" className="join-submit-button">
           가입하기
